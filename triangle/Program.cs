5fc89a2f51a5ab5332b64c6e22a0317153501797
Administrator <Administrator@DESKTOP-VDVDNER>
2024-01-09 18:31:26 +0400

    Add project files.

---
+++ triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class DrawTriangle
+{
+    public static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int width = Convert.ToInt32(Console.ReadLine());
+        int height = width;
+
+        for (int row = 0; row < height; row++)
+        {
+            for ( int column = 0; column < width; column++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            width--;
+        }
+    }
+}
+
+
+
